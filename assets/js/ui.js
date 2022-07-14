380b47a4cf5140c478c8ef30ef9d53def0a5313d
timokonkwo <chibuiketimokonkwo@gmail.com>
2022-07-14 19:35:24 +0100

    ui

---
+++ assets/js/ui.js
@@ -0,0 +1,44 @@
+/* Saved Recipes state
+        <div class="card bg-light mb-3 mt-3 mx-auto" style="max-width: 30rem;">
+            <div class="card-header">Saved Recipes</div>
+
+            <div class="card-body">
+                <div class="recipe accordion">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#recipe-1" aria-expanded="false">
+                              Egusi soup
+                            </button>
+                        </h2>
+                        <div id="recipe-1" class="accordion-collapse collapse">
+                            <div class="accordion-body">
+                                <p class="comments">Hello this is an amazing recipe Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates quia debitis nihil asperiores fugit consectetur doloribus adipisci odio. Aliquid officia sunt a harum, nam ratione quidem
+                                    facere impedit odit amet?
+                                </p>
+
+                                <div class='d-flex justify-content-between'>
+                                    <p class="author text-muted">Tim</p>
+                                    <p class="time text-muted">5/5/2022</p>
+                                </div>
+
+
+                                <a href="#" class="edit card-link" data-id="${post.id}">
+                                    <i class="fa fa-pencil"></i>
+                                </a>
+
+                                <a href="#" class="delete card-link" data-id="${post.id}">
+                                    <i class="fa fa-remove"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <button type="button" class="btn btn-danger rounded shadow-none mt-4">Delete All</button>
+
+                    <button type="button" class="btn btn-primary rounded shadow-none mt-4 float-end">Back</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        */
